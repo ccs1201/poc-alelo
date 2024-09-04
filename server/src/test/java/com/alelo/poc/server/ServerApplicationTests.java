8ab2b59be844292d059ebbecf6caa47b33ed66e5
cleber.souza <ccs1201@gmail.com>
2024-09-04 18:48:42 -0300

    #2 - Habilitado virtual threads e Implementado modulo de teste de cara

--- server/src/test/java/com/alelo/poc/server/ServerApplicationTests.java
+++ server/src/test/java/com/alelo/poc/server/ServerApplicationTests.java
@@ -1,14 +1,37 @@
 package com.alelo.poc.server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class ServerApplicationTests {
+
+    private static final int QTD_TESTS = 100_000;
+
+    @LocalServerPort
+    private Integer port;
+
+    @Autowired
+    private WebTestClient webTestClient;
 
     @Test
     void contextLoads() {
-        // Test case to ensure the application context loads successfully
+        for (int i = 0; i < QTD_TESTS; i++) {
+            CompletableFuture.runAsync(() ->
+                            webTestClient.post()
+                                    .uri("http://localhost:" + port + "/pagamentos")
+                                    .exchange()
+                                    .expectStatus().isCreated()
+                    , Executors.newVirtualThreadPerTaskExecutor());
+        }
     }
 
 }
