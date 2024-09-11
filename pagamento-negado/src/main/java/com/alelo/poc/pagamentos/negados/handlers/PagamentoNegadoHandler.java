e96f75c94d62ed02b501131f890c223f8edcd4e0
cleber.souza <ccs1201@gmail.com>
2024-09-11 16:52:30 -0300

    #19 - Ajustes

--- pagamento-negado/src/main/java/com/alelo/poc/pagamentos/negados/handlers/PagamentoNegadoHandler.java
+++ pagamento-negado/src/main/java/com/alelo/poc/pagamentos/negados/handlers/PagamentoNegadoHandler.java
@@ -13,8 +13,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import java.security.SecureRandom;
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
@@ -22,27 +20,19 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Slf4j
 public class PagamentoNegadoHandler {
 
-    private static final Random random = new SecureRandom();
     private final AtomicInteger msgProcessadosComSucesso = new AtomicInteger();
-    private final AtomicInteger tentativas = new AtomicInteger();
     private final RabbitTemplate rabbitTemplate;
 
     @PreDestroy
     public void printStats() {
         log.info("Total de mensagens processadas com sucesso: {}", msgProcessadosComSucesso.get());
-        log.info("Total de tentativas de processamento: {}", tentativas.get());
     }
 
     @RabbitListener(queues = PagamentoConstants.QUEUE_PAGAMENTO_NEGADO, concurrency = "10")
     public void handle(@Payload PagamentoEventTest event) {
-        tentativas.incrementAndGet();
-
-        if (random.nextBoolean()) {
-            rabbitTemplate.convertAndSend(PagamentoConstants.QUEUE_NOTIFICACAO_PAGAMENTO,
-                    new NotificationEvent(event.idCartao(), event.valor(), TipoNotificaoEnum.PAGAMENTO_NEGADO));
-        }
-
         log.info("Evento recebido: {}", event);
         msgProcessadosComSucesso.incrementAndGet();
+        rabbitTemplate.convertAndSend(PagamentoConstants.QUEUE_NOTIFICACAO_PAGAMENTO,
+                new NotificationEvent(event.idCartao(), event.valor(), TipoNotificaoEnum.PAGAMENTO_NEGADO));
     }
 }
